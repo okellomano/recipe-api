56f988a6f855b4fb5fca0ca3abd939af0c85deef
okellomano <onyandookello@gmail.com>
2025-01-02 08:20:25 +0300

    Implemented deleting tags

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -33,6 +33,7 @@ class RecipeViewSet(viewsets.ModelViewSet):
 
 class TagViewSet(
     mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin,
     mixins.ListModelMixin,
     viewsets.GenericViewSet
 ):
